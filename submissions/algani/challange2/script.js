86af921a7f72e2f036f11da4d08ed2d99cd75dfe
Muhammad Alghani <alghani.10922008@mahasiswa.unikom.ac.id>
2025-01-19 17:33:11 +0700

    done challange

---
+++ submissions/algani/challange2/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            targetElement.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
